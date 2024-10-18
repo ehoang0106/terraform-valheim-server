931e514caeaef0a88a3519ebeee7ca422ef00a92
Khoa Hoang <khoahoang1698@gmail.com>
2024-10-18 10:30:00 -0700

    fix varialbes.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,4 @@
-variable "access_key" {
-    description = "value of access"
-}
 
-variable "secret_key" {
-    description = "value of secret"   
-}
 
 variable "instance_type" {
     default = "t3.small"
